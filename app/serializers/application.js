b5da2b83b0b664eb303a0400d9c86c220fe74ea4
Joel AZEMAR <joel.azemar@gmail.com>
2017-08-01 13:30:43 +0200

    underscore attributes

---
+++ app/serializers/application.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  keyForAttribute(attr) {
+    return Ember.String.underscore(attr);
+  },
+
+  serialize(snapshot, options) {
+    let json = this._super(...arguments);
+
+    // const util = require('util')
+    // console.log("JSON ATTRIBUTES : " + util.inspect(json))
+
+    // ,
+
+    console.log("JSON ATTRIBUTES : " + (JSON.stringify(json, null, 4)))
+
+    return json;
+  },
+});
