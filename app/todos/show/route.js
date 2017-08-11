b6db9d9de788244817a445b1f495c616ab207452
Joel AZEMAR <joel.azemar@gmail.com>
2017-08-11 18:24:15 +0200

    Fix loading items

--- app/todos/show/route.js
+++ app/todos/show/route.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('todo', params.todo_id);
-  }
+  },
+  afterModel(model) { model.get('items').then(() => {})}
 });
