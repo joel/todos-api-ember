d4675d61f839efbbd411a0da19ef9da1d363f82e
Joel AZEMAR <joel.azemar@gmail.com>
2017-08-11 13:45:51 +0200

    Add item model

--- app/todo/model.js
+++ app/todo/model.js
@@ -4,6 +4,7 @@ import Ember from 'ember';
 export default DS.Model.extend({
   title: DS.attr('string'),
   createdBy: DS.attr('string'),
+  items: DS.hasMany('item'),
 
   isValid: Ember.computed.notEmpty('title', 'createdBy')
 });
